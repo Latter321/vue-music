e123041093d504ae738077bce7dd2643c6dfff11
lijinrong <2561071197@qq.com>
2017-11-17 18:00:44 +0800

    -ing

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="m-header">
+      <div class="icon"></div>
+      <div class="text">Chicken Music</div>
+    </div>
+</template>
+
+<script scoped type="text/ecmascript-6">
+    export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable.styl"
+  @import "~common/stylus/mixin.styl"
+
+  .m-header
+    position: relative;
+    .icon
+      color: #000
+    .text
+      color: #000
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
-
+import fastclik from 'fastclick'
 import 'common/stylus/index.styl'
+
+// fastclick的用法，绑定在body上，body中所有元素的click事件都不会再出现300毫秒延迟
+fastclik.attach(document.body)
 
 Vue.config.productionTip = false
 
