f9df4c79e044d90c4cbd86691b9f95a573585bb6
lijinrong <2561071197@qq.com>
2017-11-20 18:09:28 +0800

    the compnont singer is doing

---
+++ src/api/singer.js
@@ -0,0 +1,20 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    platform: 'ycc',
+    needNewCode: 0,
+    hostUin: 0,
+    notice: 0,
+    g_tk: 1905282013
+  })
+  return jsonp(url, data, options)
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,13 +1,91 @@
 <template>
-    <div class="">
-      this is singer
+    <div class="singer">
+      <list-view :data="singers"></list-view>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {}
+    import Singer from 'common/js/singer'
+    import ListView from 'base/listview/listview'
+    import {getSingerList} from 'api/singer'
+    import {ERR_OK} from 'api/config'
+
+    const HOT_NAME = '热门'
+    const HOT_SINGER_LEN = 10
+
+    export default {
+      data () {
+        return {
+          singers: []
+        }
+      },
+      created () {
+        this._getSingerList()
+      },
+      methods: {
+        _getSingerList () {
+          getSingerList().then((res) => {
+            if (res.code === ERR_OK) {
+              this.singers = this._normallizeSinger(res.data.list)
+              console.log(this.singers)
+            }
+          })
+        },
+        _normallizeSinger (list) {
+          let map = {
+            hot: {
+              title: HOT_NAME,
+              items: []
+            }
+          }
+
+          list.forEach((item, index) => {
+            if (index < HOT_SINGER_LEN) {
+              map.hot.items.push(new Singer({
+                id: item.Fsinger_mid,
+                name: item.Fsinger_name
+              }))
+            }
+            const key = item.Findex
+            if (!map[key]) {
+              map[key] = {
+                title: key,
+                items: []
+              }
+            }
+            map[key].items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          })
+          // 转为有序列表，处理map
+          let hot = []
+          let ret = []
+          for (let key in map) {
+            let val = map[key]
+            if (val.title.match(/[a-zA-Z]/)) {
+              ret.push(val)
+            } else if (val.title === HOT_NAME) {
+              hot.push(val)
+            }
+          }
+          // A到Z的升序
+          ret.sort((a, b) => {
+            return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+          })
+          return hot.concat(ret)
+        }
+      },
+      components: {
+        ListView
+      }
+    }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
