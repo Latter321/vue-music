a3bd56c325ec6b57f99185786ff3959f76c32355
lijinrong <2561071197@qq.com>
2017-11-27 16:25:22 +0800

    the progress-circle component is done

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -63,7 +63,9 @@
               <p class="desc" v-html="currentSong.singer"></p>
             </div>
             <div class="control">
-              <i :class="miniIcon" @click.stop="togglePlaying"></i>
+              <progress-circle :radius="radius" :percent="percent">
+                <i class="icon-mini" :class="miniIcon" @click.stop="togglePlaying"></i>
+              </progress-circle>
             </div>
             <div class="control">
               <i class="icon-playlist"></i>
@@ -79,12 +81,15 @@
     import {prefixStyle} from 'common/js/dom'
     import animations from 'create-keyframe-animation'
     import ProgressBar from 'base/progress-bar/progress-bar'
+    import ProgressCircle from 'base/progress-circle/progress-circle'
+
     const transform = prefixStyle('transform')
     export default {
       data () {
         return {
           songReady: false,
-          currentTime: 0
+          currentTime: 0,
+          radius: 32
         }
       },
       computed: {
@@ -244,7 +249,8 @@
         }
       },
       components: {
-        ProgressBar
+        ProgressBar,
+        ProgressCircle
       }
     }
 </script>
